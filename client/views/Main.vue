631aa48580c1a26b95f83b9ac33a57a9165fd52a
Pranab Kalita <pavankalita@gmail.com>
2020-05-03 18:16:06 +0530

    Degisn the Register Page

--- client/views/Main.vue
+++ client/views/Main.vue
@@ -2,10 +2,11 @@
     <div>
         <div class="h-2 w-full bg-gold-light"></div>
         <div class="w-full h-12 flex items-center justify-between px-6">
-            <div class="text-gold">MEVN</div>
+            <router-link class="text-gold" to="/">MEVN</router-link>
+
             <div class="">
                 <router-link class="text-brown" to="/auth/login">Sign In</router-link>
-                <router-link class="text-brown border-2 boder-brown p-1 ml-3 rounded-full" to="/auth/register">Join Now</router-link>
+                <router-link class="text-brown border-2 boder-brown px-3 py-2 ml-3 rounded-full hover:text-brown-darkest hover:border-brown-darkest" to="/auth/register">Join Now</router-link>
             </div>
         </div>
 
--- client/views/Register.vue
+++ client/views/Register.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>Register Here</h1>
+    <div class="container w-full h-12 mx-auto my-16">
+        <div class="max-w-sm mx-auto h-12">
+            <h2 class="text-gold text-center text-xl">Register</h2>
+
+            <div class="w-full bg-white shadow mt-5 rounded-sm p-8">
+                <input type="text" class="w-full text-sm bg-brown-lightest p-3 focus:outline-none text-brown mb-3" placeholder="Enter your name">
+
+                <input type="email" class="w-full text-sm bg-brown-lightest p-3 focus:outline-none text-brown mb-3" placeholder="Enter your email">
+
+                <input type="password" class="w-full text-sm bg-brown-lightest p-3 focus:outline-none text-brown" placeholder="Enter your password">
+
+                <button class="w-full text-sm mt-3 py-3 bg-emerald text-white rounded-sm focus:outline-none hover:bg-emerald-light">Sign Up</button>
+            </div>
+        </div>
+    </div>
 </template>
