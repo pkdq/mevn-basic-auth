c655d153bc82c8d72c09c8aa0c3b301088aa8c19
Pranab Kalita <pavankalita@gmail.com>
2020-05-03 15:57:58 +0530

    Setup Client SSR using webpack dev middleware

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import Path from 'path'
 import Express from 'express'
 import Mongoose from 'mongoose'
 import BodyParser from 'body-parser' 
@@ -6,22 +7,37 @@ import BodyParser from 'body-parser'
 import config from '@config'
 import v1Router from '@routes'
 
+// Client Setup Imports
+import Webpack from 'webpack'
+import WebpackConfig from './../webpack.config'
+import WebpackDevMiddleware from 'webpack-dev-middleware'
+
+// Establish connection to the MondoDB
 Mongoose.connect(config.database_url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 Mongoose.set('useFindAndModify', false)
 
+// Create new ExpressJS Instance
 const app = Express()
 
+// Setup Client Server-Side Rendering (will launch our Client Side)
+const compiler = Webpack(WebpackConfig)
+app.use(WebpackDevMiddleware(compiler))
+
+// To Parse incoming request bodies. Available under the req.body property 
 app.use(BodyParser.json())
 
+// Register all the routes
 app.use(v1Router)
 
+// Wildcard Route to handle non-existing routes (client will handle the SPA routes)
 app.get('*', (req, res) => {
-    return res.status(200).json({ message: 'Here client index page will be rendered !' })
+    return res.sendFile(Path.resolve(__dirname, 'public/index.html'))
 })
 
+// Create server and listen to a PORT
 app.listen(3000, () => {
     console.log('Server Started !')
 })
