586e940181314bbe537c6b4be07be867e70f476c
Pranab Kalita <pavankalita@gmail.com>
2020-05-03 19:50:38 +0530

    Creating a common custom Input Text Component

---
+++ client/components/common/cTextInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <input 
+        :type="type"
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+        class="w-full text-sm bg-brown-lightest p-3 focus:outline-none text-brown mb-3" 
+        :placeholder="placeholder">
+</template>
+
+<script>
+export default {
+    name: 'cTextInput',
+    props: {
+        placeholder: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: false,
+            default: 'text'
+        },
+        value: {
+            type: String,
+            required: false,
+            default: ''
+        }
+    },
+}
+</script>
--- client/views/Register.vue
+++ client/views/Register.vue
@@ -4,14 +4,33 @@
             <h2 class="text-gold text-center text-xl">Register</h2>
 
             <div class="w-full bg-white shadow mt-5 rounded-sm p-8">
-                <input type="text" class="w-full text-sm bg-brown-lightest p-3 focus:outline-none text-brown mb-3" placeholder="Enter your name">
+                <c-text-input v-model="model.name" placeholder="Enter your name" />
 
-                <input type="email" class="w-full text-sm bg-brown-lightest p-3 focus:outline-none text-brown mb-3" placeholder="Enter your email">
+                <c-text-input v-model="model.email" type="email" placeholder="Enter your email" />
 
-                <input type="password" class="w-full text-sm bg-brown-lightest p-3 focus:outline-none text-brown" placeholder="Enter your password">
+                <c-text-input v-model="model.password" type="password" placeholder="Enter your password" />
 
                 <button class="w-full text-sm mt-3 py-3 bg-emerald text-white rounded-sm focus:outline-none hover:bg-emerald-light">Sign Up</button>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+import cTextInput from '@components/common/cTextInput.vue'
+
+export default {
+    name: 'Register',
+    components: {
+        cTextInput
+    },
+    
+    data: () => ({
+        model: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    })
+}
+</script>
